f0ff0d7beae60891a14ca4884bb7eeee5a94391a
cheki <borna986@gmail.com>
2020-11-22 23:12:00 +0100

    multiplexing

--- Openssl_EncryptDecrypt.php
+++ Openssl_EncryptDecrypt.php
@@ -3,81 +3,81 @@ class Openssl_EncryptDecrypt {
     private const CYPHER     = 'AES-256-CBC';
     private const OPTIONS    = OPENSSL_RAW_DATA;
     private const HASH_ALGO  = 'sha256';
-    private const HASH_LEN    = 32;
-
+    private const HASH_LEN   = 32;
 
     public function fetch_key(){
         try {
-            if(! file_exists('.success')){
-                $key = base64_encode(
-                    openssl_random_pseudo_bytes(
-                        openssl_cipher_iv_length(
-                            self::CYPHER
-                        )
-                    )
-                 );
-                file_put_contents('.success', $key);
-                echo "new key saved.\n";
-            } else {
-                $key = file_get_contents(".success");
+            if(file_exists('.env') === false){
+                echo "crypto-key not found.\n";
+                return false;
+            } else { 
+                if(($key = file_get_contents(".env")) === false){
+                    echo 'error reading crypto-key.';
+                    return false;
+                }
             }
-        } catch(\Exception $e) {
+        } catch(\Throwable $e) {
             throw $e;
             return false;
         }
         return base64_decode($key);
     }
-
-    public function encrypt_cbc($clrtext, $key){
+    /**
+     * 
+     * @param msg  => plaintext
+     * @param iv   - len => 128 bits
+     * @param hmac - len => 256 bits
+     * 
+     * @return \{hex}$iv.{hex}$hmac.{base64}$ciphertext | false
+     */
+    public function encrypt_cbc($clrtext){
+        if(($base64key = self::fetch_key()) === null){
+            return false;
+        }
+        $key = base64_decode($base64key);
         try {
-            $ivlen          = openssl_cipher_iv_length(self::CYPHER);
-            $iv             = openssl_random_pseudo_bytes($ivlen);
-            $ciphertext_raw = openssl_encrypt($clrtext, self::CYPHER, $key, self::OPTIONS, $iv);
-            $hmac           = hash_hmac(self::HASH_ALGO, $ciphertext_raw, $key, true);
-    
-            return base64_encode($iv.$hmac.$ciphertext_raw);
+            $ivlen      = openssl_cipher_iv_length(self::CYPHER);
+            $iv         = openssl_random_pseudo_bytes($ivlen);
+            $ciphertext = openssl_encrypt($clrtext, self::CYPHER, $key, self::OPTIONS, $iv);
+            $hmac       = hash_hmac(self::HASH_ALGO, $iv.$ciphertext, $key, true);
+            return base64_encode($iv.$hmac.$ciphertext);
         } catch (\Exception $e){
             throw $e;
             return false;
         }
-
-        echo '!! not supposed to be here !!';
         return false;
     }
 
-    public function decrypt_cbc($encrypted, $key){
-        $encrypted = base64_decode($encrypted);
-
-        if(! $encrypted || empty($encrypted)){
-            return null;
+    public function decrypt_cbc($encrypted){
+        if(($base64key = self::fetch_key()) === null){
+            return false;
         }
+        if($encrypted === false || empty($encrypted)){
+            return false;
+        }
+        $key = base64_decode($base64key);
+        $encrypted = base64_decode($encrypted);
         try {
-            $ivlen          = openssl_cipher_iv_length(self::CYPHER);
-            $iv             = substr($encrypted, 0, $ivlen);
-            $hmac           = substr($encrypted, $ivlen, self::HASH_LEN);
-            $ciphertext_raw = substr($encrypted, ($ivlen+self::HASH_LEN));
-            $clrtext        = @openssl_decrypt($ciphertext_raw, self::CYPHER, $key, self::OPTIONS, $iv);
+            $ivlen      = openssl_cipher_iv_length(self::CYPHER);
+            $iv         = substr($encrypted, 0, $ivlen);
+            $hmac       = substr($encrypted, $ivlen, self::HASH_LEN);
+            $ciphertext = substr($encrypted, ($ivlen+self::HASH_LEN));
+            $clrtext    = openssl_decrypt($ciphertext, self::CYPHER, $key, self::OPTIONS, $iv);
             
             if($clrtext === false){
                 return false;
             }
-            $calcmac = hash_hmac(self::HASH_ALGO, $ciphertext_raw, $key, true);
-
+            $calcmac = hash_hmac(self::HASH_ALGO, $iv.$ciphertext, $key, true);
             if(function_exists('hash_equals')) {
-                if (hash_equals($hmac, $calcmac)){
-                    return $clrtext;
-                }
+                if (hash_equals($hmac, $calcmac)){ return $clrtext; }
             } else {
-                if ($this->hash_equals_custom($hmac, $calcmac)){
-                    return $clrtext;
-                }
+                if ($this->hash_equals_custom($hmac, $calcmac)){ return $clrtext; }
             }
+            return false;
         } catch (\Exception $e){
             throw $e;
             return false;
         }
-
-        echo '!! not supposed to be here !!';
         return false;
     }
     /**
